1d8839c21efc038316e3ab6cfbbb9f975e464b84
ZakWang66 <44783623+ZakWang66@users.noreply.github.com>
2020-02-25 18:01:42 -0500

    v 1.0

--- frontend-sinatra/app.rb
+++ frontend-sinatra/app.rb
@@ -1,12 +1,19 @@
 # entry point
 require 'sinatra'
+require 'net/http'
+require 'json'
 
 # retrieve your api host address like this
 #   settings.api => "someaddress.ondigitalocean.com"
-set :api, ENV['API_HOST']
-set :api_port, ENV['API_PORT'] || 80
+# set :api, ENV['API_HOST']
+# set :api_port, ENV['API_PORT'] || 80
 
 # Show a pretty table of all users... rendered on a html page.
 get '/' do
+  url = "http://#{ENV['API_HOST']}:#{ENV['API_PORT']}"
+  @url_users = "#{url}/users"
+  @url_users_destroy = "#{url}/users/destroy"
+
+  @data = JSON.parse(Net::HTTP.get(URI(@url_users)))
   erb :index
 end
--- service-api-sinatra/app.rb
+++ service-api-sinatra/app.rb
@@ -1,20 +1,25 @@
 # entry point
 require 'sinatra'
+require 'faker'
 
 require_relative './models/user'
 
 # Retrieve all users from database.
 get '/users' do
-  'Implement me!'
+  User.all.to_json
 end
 
 # Create n users.
 post '/users' do
-  count = params[:n].to_i || 1
-  'Implement me!'
+  count = params[:count].to_i || 1
+  for i in 1..count do
+    User.create(name:Faker::Name.name, email:Faker::Internet.email, bio:Faker::Address.street_address, password:Faker::Address.zip)
+  end
+  redirect "http://#{ENV['FRONT_URL']}"
 end
 
 # Delete all users.
 post '/users/destroy' do
-  'Implement me!'
+  User.destroy_all
+  redirect "http://#{ENV['FRONT_URL']}"
 end
